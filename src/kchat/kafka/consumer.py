e623f6fa688507bb3d7b3f21c6e8bd3a8ecee7fe
NishNovae <raecrowned@gmail.com>
2024-08-21 16:50:55 +0900

    offset handling

--- src/kchat/kafka/consumer.py
+++ src/kchat/kafka/consumer.py
@@ -1,19 +1,46 @@
 # src/kchat/kafka/consumer.py
 
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition
 from json import loads
+import os
+
+OFFSET_FILE = 'consumer_offset.txt'
+
+def save_offset(offset):
+    with open(OFFSET_FILE, 'w') as file:
+        file.write(str(offset))
+
+def read_offset():
+    if os.path.exists(OFFSET_FILE):
+        with open(OFFSET_FILE, 'r') as file:
+            return int(file.read().strip())
+    return None
+
 
 consumer = KafkaConsumer(
-    'topic1',
+#    'topic1',
    # configs = {},
     bootstrap_servers = ['localhost:9092'],
     value_deserializer = lambda x: loads(x.decode('utf-8')),    
-    consumer_timeout_ms = 5000
+    consumer_timeout_ms = 5000,
+    auto_offset_reset='earliest' if read_offset() is None else 'none',
+    group_id = 'fbi',
+    enable_auto_commit=True
 )
 
 print('[START] get consumer')
 
-for  message in consumer:
-    print(message)
+saved_offset = read_offset()
+if saved_offset is not None:
+    p = TopicPartition('topic1', 0)
+    consumer.assign([p])    # assign partition
+    consumer.seek(p, saved_offset)
+
+
+
+for  m in consumer:
+    print(f"offset={m.offset}, value={m.value}")
+    save_offset(m.offset +1)
+#    print(f"topic={m.topic}, partition={m.partition}, offset={m.offset}, value={m.value}, timestamp={m.timestamp}")
 
 print('[END] get consumer')
